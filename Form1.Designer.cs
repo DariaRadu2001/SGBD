36aa68192ebd60c92ca21456bbe2b865a4bcbe85
Daria <80980261+DariaRadu2001@users.noreply.github.com>
2022-04-07 17:48:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace Laborator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.dataGridView_Adresse = new System.Windows.Forms.DataGridView();
+            this.dataGridView_Kunde = new System.Windows.Forms.DataGridView();
+            this.Adresse = new System.Windows.Forms.Label();
+            this.Runway = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_CL_ID = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBoxAdresse_ID = new System.Windows.Forms.TextBox();
+            this.Insert_button = new System.Windows.Forms.Button();
+            this.Delete_button = new System.Windows.Forms.Button();
+            this.Update_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Adresse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Kunde)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.BackColor = System.Drawing.Color.RosyBrown;
+            this.Connect.FlatAppearance.BorderSize = 0;
+            this.Connect.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Connect.ForeColor = System.Drawing.Color.Black;
+            this.Connect.Location = new System.Drawing.Point(12, 31);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(127, 38);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = false;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // dataGridView_Adresse
+            // 
+            this.dataGridView_Adresse.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView_Adresse.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView_Adresse.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView_Adresse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Adresse.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView_Adresse.Location = new System.Drawing.Point(171, 58);
+            this.dataGridView_Adresse.Name = "dataGridView_Adresse";
+            this.dataGridView_Adresse.RowHeadersWidth = 51;
+            this.dataGridView_Adresse.Size = new System.Drawing.Size(300, 172);
+            this.dataGridView_Adresse.TabIndex = 1;
+            this.dataGridView_Adresse.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Adresse_CellContentClick);
+            // 
+            // dataGridView_Kunde
+            // 
+            this.dataGridView_Kunde.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView_Kunde.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView_Kunde.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView_Kunde.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Kunde.Location = new System.Drawing.Point(171, 263);
+            this.dataGridView_Kunde.Name = "dataGridView_Kunde";
+            this.dataGridView_Kunde.RowHeadersWidth = 51;
+            this.dataGridView_Kunde.Size = new System.Drawing.Size(300, 176);
+            this.dataGridView_Kunde.TabIndex = 2;
+            this.dataGridView_Kunde.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_Kunde_CellContentClick);
+            // 
+            // Adresse
+            // 
+            this.Adresse.AutoSize = true;
+            this.Adresse.BackColor = System.Drawing.Color.RosyBrown;
+            this.Adresse.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Adresse.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adresse.Location = new System.Drawing.Point(171, 31);
+            this.Adresse.Name = "Adresse";
+            this.Adresse.Size = new System.Drawing.Size(99, 29);
+            this.Adresse.TabIndex = 3;
+            this.Adresse.Text = "Adresse\r\n";
+            // 
+            // Runway
+            // 
+            this.Runway.AutoSize = true;
+            this.Runway.BackColor = System.Drawing.Color.RosyBrown;
+            this.Runway.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Runway.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Runway.Location = new System.Drawing.Point(171, 236);
+            this.Runway.Name = "Runway";
+            this.Runway.Size = new System.Drawing.Size(83, 29);
+            this.Runway.TabIndex = 4;
+            this.Runway.Text = "Kunde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Eras Demi ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CL_ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Eras Demi ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(504, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Eras Demi ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(504, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adresse_ID";
+            // 
+            // textBox_CL_ID
+            // 
+            this.textBox_CL_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_CL_ID.Location = new System.Drawing.Point(581, 262);
+            this.textBox_CL_ID.Name = "textBox_CL_ID";
+            this.textBox_CL_ID.Size = new System.Drawing.Size(141, 26);
+            this.textBox_CL_ID.TabIndex = 8;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox_Name.Location = new System.Drawing.Point(581, 296);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(140, 26);
+            this.textBox_Name.TabIndex = 9;
+            // 
+            // textBoxAdresse_ID
+            // 
+            this.textBoxAdresse_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxAdresse_ID.Location = new System.Drawing.Point(604, 332);
+            this.textBoxAdresse_ID.Name = "textBoxAdresse_ID";
+            this.textBoxAdresse_ID.Size = new System.Drawing.Size(117, 26);
+            this.textBoxAdresse_ID.TabIndex = 10;
+            // 
+            // Insert_button
+            // 
+            this.Insert_button.BackColor = System.Drawing.Color.RosyBrown;
+            this.Insert_button.FlatAppearance.BorderSize = 0;
+            this.Insert_button.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insert_button.ForeColor = System.Drawing.Color.Black;
+            this.Insert_button.Location = new System.Drawing.Point(502, 367);
+            this.Insert_button.Name = "Insert_button";
+            this.Insert_button.Size = new System.Drawing.Size(90, 31);
+            this.Insert_button.TabIndex = 11;
+            this.Insert_button.Text = "Insert";
+            this.Insert_button.UseVisualStyleBackColor = false;
+            this.Insert_button.Click += new System.EventHandler(this.Insert_button_Click);
+            // 
+            // Delete_button
+            // 
+            this.Delete_button.BackColor = System.Drawing.Color.RosyBrown;
+            this.Delete_button.FlatAppearance.BorderSize = 0;
+            this.Delete_button.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete_button.ForeColor = System.Drawing.Color.Black;
+            this.Delete_button.Location = new System.Drawing.Point(598, 367);
+            this.Delete_button.Name = "Delete_button";
+            this.Delete_button.Size = new System.Drawing.Size(92, 31);
+            this.Delete_button.TabIndex = 12;
+            this.Delete_button.Text = "Delete";
+            this.Delete_button.UseVisualStyleBackColor = false;
+            this.Delete_button.Click += new System.EventHandler(this.Delete_button_Click);
+            // 
+            // Update_button
+            // 
+            this.Update_button.BackColor = System.Drawing.Color.RosyBrown;
+            this.Update_button.FlatAppearance.BorderSize = 0;
+            this.Update_button.Font = new System.Drawing.Font("Eras Medium ITC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update_button.ForeColor = System.Drawing.Color.Black;
+            this.Update_button.Location = new System.Drawing.Point(696, 367);
+            this.Update_button.Name = "Update_button";
+            this.Update_button.Size = new System.Drawing.Size(88, 31);
+            this.Update_button.TabIndex = 13;
+            this.Update_button.Text = "Update";
+            this.Update_button.UseVisualStyleBackColor = false;
+            this.Update_button.Click += new System.EventHandler(this.Update_button_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(796, 481);
+            this.Controls.Add(this.Update_button);
+            this.Controls.Add(this.Delete_button);
+            this.Controls.Add(this.Insert_button);
+            this.Controls.Add(this.textBoxAdresse_ID);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.textBox_CL_ID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Runway);
+            this.Controls.Add(this.Adresse);
+            this.Controls.Add(this.dataGridView_Kunde);
+            this.Controls.Add(this.dataGridView_Adresse);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Adresse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Kunde)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.DataGridView dataGridView_Adresse;
+        private System.Windows.Forms.DataGridView dataGridView_Kunde;
+        private System.Windows.Forms.Label Adresse;
+        private System.Windows.Forms.Label Runway;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_CL_ID;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBoxAdresse_ID;
+        private System.Windows.Forms.Button Insert_button;
+        private System.Windows.Forms.Button Delete_button;
+        private System.Windows.Forms.Button Update_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Laborator1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString =
+       "Data Source=DARIA\\SQLEXPRESS;Initial Catalog=Labor1;"
+       + "Integrated Security=true";
+
+        public Form1()
+        {
+            InitializeComponent();
+            inputReset();
+        }
+
+        private void inputReset()
+        {
+            textBox_CL_ID.Clear();
+            textBox_Name.Clear();
+            textBoxAdresse_ID.Clear();
+
+            textBox_CL_ID.Enabled = false;
+            textBox_Name.Enabled = false;
+            textBoxAdresse_ID.Enabled = false;
+           
+            Insert_button.Enabled = false;
+            Update_button.Enabled = false;
+            Delete_button.Enabled = false;
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter adresseAdapter = new SqlDataAdapter("select * from Adresa", connectionString);
+            SqlDataAdapter kundeAdapter = new SqlDataAdapter("select * from Clienti", connectionString);
+
+            adresseAdapter.Fill(dataSet, "Adresa");
+            kundeAdapter.Fill(dataSet, "Clienti");
+
+            //aici fac relatia dintre copil si parinte
+            DataRelation dataRelation = new DataRelation("FK_Clienti_Adresa",
+                dataSet.Tables["Adresa"].Columns["Ad_ID"],
+                dataSet.Tables["Clienti"].Columns["Ad_ID"]
+                
+               );
+
+            try
+            {
+                dataSet.Relations.Add(dataRelation);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            //aici se fac legaturile pt dataGridView
+            BindingSource bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = dataSet; 
+            bindingSource1.DataMember = "Adresa";
+
+            BindingSource bindingSource2 = new BindingSource();
+            bindingSource2.DataSource = bindingSource1;
+            bindingSource2.DataMember = "FK_Clienti_Adresa";
+
+
+            dataGridView_Adresse.DataSource = bindingSource1;
+            dataGridView_Kunde.DataSource = bindingSource2;
+
+            
+        }
+
+        private void dataGridView_Adresse_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                inputReset();
+
+                string ad_ID = this.dataGridView_Adresse[0, this.dataGridView_Adresse.CurrentCell.RowIndex].Value.ToString();
+
+                //le dau enable pt insert
+                if (!String.IsNullOrWhiteSpace(ad_ID))
+                {
+                    textBoxAdresse_ID.Text = ad_ID;
+                    textBox_CL_ID.Enabled = true;
+                    textBox_Name.Enabled = true;
+                    Insert_button.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //iau datele de la fiecare tupel din copil
+        private void dataGridView_Kunde_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                inputReset();
+
+                int rowNumber = this.dataGridView_Kunde.CurrentCell.RowIndex;
+
+                string cl_id = this.dataGridView_Kunde[0, rowNumber].Value.ToString();
+                string name = this.dataGridView_Kunde[1, rowNumber].Value.ToString();
+                string adresse_id = this.dataGridView_Kunde[2, rowNumber].Value.ToString();
+
+
+                if (!String.IsNullOrWhiteSpace(cl_id))
+                {
+                    textBoxAdresse_ID.Text = adresse_id;
+                    textBox_CL_ID.Text = cl_id;
+                    textBox_Name.Text = name;
+
+                    textBox_Name.Enabled = true;
+                    textBoxAdresse_ID.Enabled = true;
+
+                    Update_button.Enabled = true;
+                    Delete_button.Enabled = true;
+                  
+                }
+                else
+                {
+                    textBox_CL_ID.Text = cl_id;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Insert_button_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+            dataAdapter.InsertCommand = new SqlCommand("INSERT INTO Clienti" +
+                " VALUES(@Cl_ID, @Name, @Ad_ID)",
+                sqlConnection);
+
+
+            try
+            {
+                sqlConnection.Open();
+                dataAdapter.InsertCommand.Parameters.Add("@Cl_ID", SqlDbType.Int).Value = Int32.Parse(textBox_CL_ID.Text);
+                dataAdapter.InsertCommand.Parameters.Add("@Name", SqlDbType.VarChar).Value = textBox_Name.Text;
+                dataAdapter.InsertCommand.Parameters.Add("@Ad_ID", SqlDbType.Int).Value = Int32.Parse(textBoxAdresse_ID.Text);
+                dataAdapter.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("Inserted data successfully.");
+                sqlConnection.Close();
+            }
+            catch(System.FormatException)
+            {
+                MessageBox.Show("Falsches ID.");
+                sqlConnection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Sie haben falsche Daten gegeben.");
+                sqlConnection.Close();
+            }
+
+            inputReset();
+            Connect_Click(sender, e);
+            
+          
+        }
+
+        private void Delete_button_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+            dataAdapter.DeleteCommand = new SqlCommand("DELETE FROM Clienti WHERE Cl_ID = @cl_ID", connection);
+            dataAdapter.DeleteCommand.Parameters.Add("@cl_ID", SqlDbType.Int).Value = Int32.Parse(textBox_CL_ID.Text);
+
+            try
+            {
+                connection.Open();
+                dataAdapter.DeleteCommand.ExecuteNonQuery();
+                MessageBox.Show("Deleted data successfully.");
+                connection.Close();
+            }
+                
+            catch (Exception ex)
+            {
+                MessageBox.Show("The inserted data are not valid.");
+                connection.Close();
+            }   
+               
+            
+            inputReset();
+            Connect_Click(sender, e);
+            
+            
+        }
+
+        private void Update_button_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+            dataAdapter.UpdateCommand = new SqlCommand("UPDATE Clienti" +
+                " SET Name = @Name , Ad_ID = @Ad_ID" +
+                " WHERE Cl_ID = @Cl_ID",
+                connection);
+
+    
+            try
+            {
+                connection.Open();
+                dataAdapter.UpdateCommand.Parameters.Add("@Cl_ID", SqlDbType.Int).Value = Int32.Parse(textBox_CL_ID.Text);
+                dataAdapter.UpdateCommand.Parameters.Add("@Name", SqlDbType.VarChar).Value = textBox_Name.Text;
+                dataAdapter.UpdateCommand.Parameters.Add("@Ad_ID", SqlDbType.Int).Value = Int32.Parse(textBoxAdresse_ID.Text);
+                dataAdapter.UpdateCommand.ExecuteNonQuery();
+                MessageBox.Show("Updated data successfully.");
+                connection.Close();
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Sie sollen ein valider Integer geben.");
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sie haben falsche Daten gegeben.");
+                connection.Close();
+            }
+             
+            inputReset();
+            Connect_Click(sender, e);
+        }
+            
+        
+    }
+}
